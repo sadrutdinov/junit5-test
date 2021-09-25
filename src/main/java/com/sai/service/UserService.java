e7d2b6b69c281381ef8182c064326ab5d33252d7
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2021-09-25 18:35:38 +0400

    4. Test Lifecycle

--- src/main/java/com/sai/service/UserService.java
+++ src/main/java/com/sai/service/UserService.java
@@ -2,14 +2,20 @@ package com.sai.service;
 
 import com.sai.dto.User;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class UserService {
 
+    private final List<User> users = new ArrayList<>();
+
 
     public List<User> getAll() {
+        return users;
+    }
 
-        return Collections.emptyList();
+    public boolean add(User user) {
+        return users.add(user);
     }
 }
--- src/test/java/com/sai/service/UserServiceTest.java
+++ src/test/java/com/sai/service/UserServiceTest.java
@@ -1,21 +1,58 @@
 package com.sai.service;
 
 import com.sai.dto.User;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // создается один объект класса UserServiceTest для всех тестов
+// @TestInstance(TestInstance.Lifecycle.PER_METHOD) - создается объект для каждого теста, @BeforeAll и @AfterAll должны быть static
 class UserServiceTest {
+
+    @BeforeAll
+    void init() {
+        System.out.println("BeforeAll: " + this);
+    }
+
+    private UserService userService;
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("BeforeEach: " + this);
+        userService = new UserService();
+    }
+
 
     @Test
     void usersEmptyIfNoUsersAdded() {
-        UserService userService = new UserService();
+        System.out.println("Test1: " + this);
 
         List<User> all = userService.getAll();
 
-        assertTrue(all.isEmpty(), ()-> "User list should be empty");
+        assertTrue(all.isEmpty(), () -> "User list should be empty");
+    }
+
+    @Test
+    void userSizeIfUsersAdded() {
+        System.out.println("Test2: " + this);
+        userService.add(new User());
+        userService.add(new User());
+
+        List<User> all = userService.getAll();
+
+        assertEquals(2, all.size());
+    }
+
+    @AfterEach
+    void deleteDateFromDatabase() {
+        System.out.println("AfterEach: " + this);
+    }
+
+    @AfterAll
+    void closeConnectionPool() {
+        System.out.println("AfterAll: " + this);
     }
 
 }
